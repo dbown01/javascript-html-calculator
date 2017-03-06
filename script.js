15cf450b850fe50accc64a4f7e15da79d6c2f27b
Drew Brown <dbrown@wlapcs.org>
2017-03-06 20:56:34 +0000

    fixing broken stuff

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function clearDisplay(){
-     document.getElementById("display").value += "";
+     document.getElementById("display-bar").innerHTML = "";
  }
 
 function display(userInput){
-document.getElementById("display-bar").value += userInput;
+document.getElementById("display-bar").innerHTML += userInput;
 }
 
 function calculate(){
  var answer = eval(document.getElementById("display-bar").innerHTML);
-document.getElementById("display-bar").value = answer;
+document.getElementById("display-bar").innerHTML = answer;
 }
 
